42d010c605b73ffc627f5f09f120fd1584138735
ibnabassalugo <ibnabassalugo@gmail.com>
2018-07-01 23:26:07 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -3,8 +3,26 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+  $("#submit").on("click", function makeGrid(event) {
 
-}
+    var r = $("#inputHeight").val();
+    var c = $("#inputWidth").val();
+  $("table").empty();
+    var grid = $("table");
+    var tr = "<tr></tr>";
+    var td = "<td></td>";
+  for (var i = 0; i < r; i+=1) {
+    $("table").append("<tr></tr>");
+    for (var j = 0; j < c; j+=1) {
+      $("tr:last-child").append("<td></td>");
+      }
+    }
+
+
+  $("table tr td").on("click", function() {
+    $(this).css("background-color", $("#colorPicker").val());
+    });
+    event.preventDefault();
+
+  });
